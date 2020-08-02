5c6cde5e05668092aacc00033017c6a428866ea3
alecsavvy <alecsavvy@outlook.com>
2020-08-02 12:49:44 -0700

    some work on contracts

--- chain/contracts/SimpleStorage.sol
+++ chain/contracts/SimpleStorage.sol
@@ -2,6 +2,11 @@ pragma solidity >=0.4.0 <0.7.0;
 
 contract SimpleStorage {
     uint256 storedData;
+    address owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
 
     function set(uint256 x) public {
         storedData = x;
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Web3 from "web3";
+import { Account } from "web3-core";
+import { AbiItem } from "web3-utils/types";
+import abi from "./abi.json";
 
 function App() {
+  const web3 = new Web3(
+    Web3.givenProvider || process.env.REACT_APP_CHAIN_WS_URL
+  );
+
+  const [account, setAccount] = useState<Account>();
+  const bytecode = SimpleStorage.bytecode;
+  const deployContract = () => {
+    const jsonInterface = abi;
+    const contract = new web3.eth.Contract(jsonInterface);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Shuffld
+      <div>Current Account: {account?.address}</div>
+      <button onClick={() => setAccount(web3.eth.accounts.create())}>
+        Create New Account
+      </button>
+      <button onClick={() => console.log("deploying contract...")}>
+        Deploy Contract
+      </button>
     </div>
   );
 }
